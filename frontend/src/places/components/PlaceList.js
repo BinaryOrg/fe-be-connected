63d42326d3f9cb6ac6054a69c687d45b5ca98d6b
Subham Sahu <about.subhamsahu@gmail.com>
2022-12-06 19:30:47 +0530

    06th Dec 2022 - S10 Done!

--- frontend/src/places/components/PlaceList.js
+++ frontend/src/places/components/PlaceList.js
@@ -2,9 +2,8 @@ import React from "react";
 
 import Card from "../../shared/components/UIElements/Card";
 import PlaceItem from "./PlaceItem";
-import "./PlaceList.css";
-
 import Button from "../../shared/components/FormComponents/Button";
+import "./PlaceList.css";
 
 const PlaceList = (props) => {
   if (props.items.length === 0) {
@@ -24,12 +23,13 @@ const PlaceList = (props) => {
         <PlaceItem
           key={place.id}
           id={place.id}
-          image={place.imageUrl}
+          image={place.image}
           title={place.title}
           description={place.description}
           address={place.address}
           creatorId={place.creator}
           coordinates={place.location}
+          onDelete={props.onDeletePlace}
         />
       ))}
     </ul>
@@ -37,3 +37,44 @@ const PlaceList = (props) => {
 };
 
 export default PlaceList;
+
+// import React from "react";
+
+// import Card from "../../shared/components/UIElements/Card";
+// import PlaceItem from "./PlaceItem";
+// import "./PlaceList.css";
+
+// import Button from "../../shared/components/FormComponents/Button";
+
+// const PlaceList = (props) => {
+//   if (props.items.length === 0) {
+//     return (
+//       <div className="place-list center">
+//         <Card>
+//           <h2>No places found. Maybe create one?</h2>
+//           <Button to="/places/new">Share Place</Button>
+//         </Card>
+//       </div>
+//     );
+//   }
+
+//   return (
+//     <ul className="place-list">
+//       {props.items.map((place) => (
+//         <PlaceItem
+//           key={place.id}
+//           id={place.id}
+//           image={place.image}
+//           title={place.title}
+//           description={place.description}
+//           address={place.address}
+//           creatorId={place.creator}
+//           coordinates={place.location}
+//           onDelete={props.onDeletePlace}
+//         />
+//       ))}
+//     </ul>
+//   );
+// };
+
+// export default PlaceList;
--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -1,22 +1,22 @@
 import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import "./PlaceForm.css";
-import Input from "../../shared/components/FormComponents/Input";
-import {
-  VALIDATOR_MINLENGTH,
-  VALIDATOR_REQUIRE,
-} from "../../shared/util/validators";
 
+import Input from "../../shared/components/FormComponents/Input";
 import Button from "../../shared/components/FormComponents/Button";
-import { AuthContext } from "../../shared/context/auth-context";
-import { useForm } from "../../shared/hooks/form-hook";
-import { useHttpClient } from "../../shared/hooks/http-hook";
 import ErrorModal from "../../shared/components/UIElements/ErrorModal";
 import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH,
+} from "../../shared/util/validators";
+import { useForm } from "../../shared/hooks/form-hook";
+import { useHttpClient } from "../../shared/hooks/http-hook";
+import { AuthContext } from "../../shared/context/auth-context";
+import "./PlaceForm.css";
 
 const NewPlace = () => {
   const auth = useContext(AuthContext);
-  const { isLoading, sendRequest, error, clearError } = useHttpClient();
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
   const [formState, inputHandler] = useForm(
     {
       title: {
@@ -51,7 +51,6 @@ const NewPlace = () => {
         }),
         { "Content-Type": "application/json" }
       );
-      // Redirect the user to a different page
       navigate("/");
     } catch (err) {}
   };
@@ -67,19 +66,17 @@ const NewPlace = () => {
           type="text"
           label="Title"
           validators={[VALIDATOR_REQUIRE()]}
-          errorText="Please enter a valid title"
+          errorText="Please enter a valid title."
           onInput={inputHandler}
         />
-
         <Input
           id="description"
           element="textarea"
           label="Description"
           validators={[VALIDATOR_MINLENGTH(5)]}
-          errorText="Please enter a valid description (at least 5 characters)"
+          errorText="Please enter a valid description (at least 5 characters)."
           onInput={inputHandler}
         />
-
         <Input
           id="address"
           element="input"
@@ -95,4 +92,104 @@ const NewPlace = () => {
     </React.Fragment>
   );
 };
+
 export default NewPlace;
+
+// import React, { useContext } from "react";
+// import { useNavigate } from "react-router-dom";
+// import "./PlaceForm.css";
+// import Input from "../../shared/components/FormComponents/Input";
+// import {
+//   VALIDATOR_MINLENGTH,
+//   VALIDATOR_REQUIRE,
+// } from "../../shared/util/validators";
+
+// import Button from "../../shared/components/FormComponents/Button";
+// import { AuthContext } from "../../shared/context/auth-context";
+// import { useForm } from "../../shared/hooks/form-hook";
+// import { useHttpClient } from "../../shared/hooks/http-hook";
+// import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+// import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+
+// const NewPlace = () => {
+//   const auth = useContext(AuthContext);
+//   const { isLoading, sendRequest, error, clearError } = useHttpClient();
+//   const [formState, inputHandler] = useForm(
+//     {
+//       title: {
+//         value: "",
+//         isValid: false,
+//       },
+//       description: {
+//         value: "",
+//         isValid: false,
+//       },
+//       address: {
+//         value: "",
+//         isValid: false,
+//       },
+//     },
+//     false
+//   );
+
+//   const navigate = useNavigate();
+
+//   const placeSubmitHandler = async (event) => {
+//     event.preventDefault();
+//     try {
+//       await sendRequest(
+//         "http://localhost:5000/api/places",
+//         "POST",
+//         JSON.stringify({
+//           title: formState.inputs.title.value,
+//           description: formState.inputs.description.value,
+//           address: formState.inputs.address.value,
+//           creator: auth.userId,
+//         }),
+//         { "Content-Type": "application/json" }
+//       );
+//       // Redirect the user to a different page
+//       navigate("/");
+//     } catch (err) {}
+//   };
+
+//   return (
+//     <React.Fragment>
+//       <ErrorModal error={error} onClear={clearError} />
+//       <form className="place-form" onSubmit={placeSubmitHandler}>
+//         {isLoading && <LoadingSpinner asOverlay />}
+//         <Input
+//           id="title"
+//           element="input"
+//           type="text"
+//           label="Title"
+//           validators={[VALIDATOR_REQUIRE()]}
+//           errorText="Please enter a valid title"
+//           onInput={inputHandler}
+//         />
+
+//         <Input
+//           id="description"
+//           element="textarea"
+//           label="Description"
+//           validators={[VALIDATOR_MINLENGTH(5)]}
+//           errorText="Please enter a valid description (at least 5 characters)"
+//           onInput={inputHandler}
+//         />
+
+//         <Input
+//           id="address"
+//           element="input"
+//           label="Address"
+//           validators={[VALIDATOR_REQUIRE()]}
+//           errorText="Please enter a valid address."
+//           onInput={inputHandler}
+//         />
+//         <Button type="submit" disabled={!formState.isValid}>
+//           ADD PLACE
+//         </Button>
+//       </form>
+//     </React.Fragment>
+//   );
+// };
+// export default NewPlace;
